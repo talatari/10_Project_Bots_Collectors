7ddc218c5d454711a119c3c9639b80ef0f1ad452
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-16 16:47:22 +0300

    Refactoring UnitMover.

--- Assets/Scripts/Unit/UnitMover.cs
+++ Assets/Scripts/Unit/UnitMover.cs
@@ -1,17 +1,10 @@
 using UnityEngine;
 
-[RequireComponent(
-    typeof(Unit))]
-
 public class UnitMover : MonoBehaviour
 {
     [SerializeField] private float _speedMove = 25f;
 
-    private Unit _unit;
     private Transform _targetPoint;
-
-    private void Start() => 
-        _unit = GetComponent<Unit>();
 
     private void Update()
     {
