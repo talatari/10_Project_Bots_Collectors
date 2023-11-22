735a6d0e1b24a8db23cca516b8f3acb692b333b6
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-22 19:47:26 +0300

    Add button Create Station.

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Station : MonoBehaviour
 {
+    [SerializeField] private int _amountResourcesForCreateStation = 3;
+    
     public int CountResources { get; set; }
 
     public event Action UnitCollectorFree;
     public event Action CountResourcesUpdate;
+    public event Action EnoughResources; 
     
     private void OnTriggerStay(Collider collider)
     {
@@ -21,8 +24,23 @@ public class Station : MonoBehaviour
             unit.UnitCollector.Resource.Destroy();
             unit.SetFree();
             
-            CountResources++;
-            CountResourcesUpdate?.Invoke();
+            IncreaseCountResources();
         }
+    }
+
+    private void IncreaseCountResources()
+    {
+        CountResources++;
+        CountResourcesUpdate?.Invoke();
+
+        if (CountResources >= _amountResourcesForCreateStation)
+            EnoughResources?.Invoke();
+    }
+
+    public void CreateStationHadle()
+    {
+        print(CountResources);
+        CountResources -= _amountResourcesForCreateStation;
+        CountResourcesUpdate?.Invoke();
     }
 }
---
+++ Assets/Scripts/UI/UICreateStationButtonHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UICreateStationButtonHandler : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Station _station;
+    [SerializeField] private GameObject _buttonCreateStation;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _station.CreateStationHadle();
+        _buttonCreateStation.SetActive(false);
+    }
+}
